1c03b6c2a3e7073465c1f012b854a3ea39333788
yangjoker <jeallen888@icloud.com>
2021-01-15 14:46:55 +0800

    使用系统自带网络框架处理请求

--- loader/GTListLoader.m
+++ loader/GTListLoader.m
@@ -12,10 +12,16 @@
     
     NSString *urlString = @"https://www.runoob.com/ios/ios-action-outlet.html";
     NSURL *listURL = [NSURL URLWithString:urlString];
-    NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
+//    NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
     NSURLSession *session = [NSURLSession sharedSession];
     
-    NSURLSessionDataTask * task = [session dataTaskWithRequest:listRequest];
+//    NSURLSessionDataTask * task = [session dataTaskWithRequest:listRequest];
+    NSURLSessionDataTask *datatask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSLog(@"");
+    }];
+    
+    [datatask resume];
+    
     NSLog(@"");
     
 }
