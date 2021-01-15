d2d6c9020d583346d5c36185caa19f0b21daf008
yangjoker <jeallen888@icloud.com>
2021-01-15 15:39:21 +0800

    use AFNetworking

--- loader/GTListLoader.m
+++ loader/GTListLoader.m
@@ -6,23 +6,33 @@
 //
 
 #import "GTListLoader.h"
+#import <AFNetworking.h>
 
 @implementation GTListLoader
--(void)loadData{
-    
-    NSString *urlString = @"https://www.runoob.com/ios/ios-action-outlet.html";
-    NSURL *listURL = [NSURL URLWithString:urlString];
+-(void)loadData {
+
+
+//    [[AFHTTPSessionManager manager] GET:@"http://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e" parameters:nil headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+//
+//    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//        NSLog(@"");
+//    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+//        NSLog(@"");
+//    }];
+
+
+	NSString *urlString = @"http://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e";
+	NSURL *listURL = [NSURL URLWithString:urlString];
 //    NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
-    NSURLSession *session = [NSURLSession sharedSession];
-    
+	NSURLSession *session = [NSURLSession sharedSession];
+
 //    NSURLSessionDataTask * task = [session dataTaskWithRequest:listRequest];
-    NSURLSessionDataTask *datatask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        NSLog(@"");
-    }];
-    
-    [datatask resume];
-    
-    NSLog(@"");
-    
+	NSURLSessionDataTask *datatask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+	         NSLog(@"");
+	}];
+
+	[datatask resume];
+
+	NSLog(@"");
 }
 @end
