d57af6e9f8f43edcec021c69b320a0ca7dfeaa5e
yangjoker <jeallen888@icloud.com>
2021-01-28 15:16:54 +0800

    静态库、动态库的制作与使用

--- SampleApp/AppDelegate.m
+++ SampleApp/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "GTStaticTest.h"
 
 @interface AppDelegate ()
 
@@ -16,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+//    static
+//    [[GTStaticTest alloc] init];
     return YES;
 }
 
