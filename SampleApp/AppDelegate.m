315362683c09241b3faf670a5565bf6e184d9459
yangjoker <jeallen888@icloud.com>
2021-01-31 15:27:52 +0800

    crash收集

--- SampleApp/AppDelegate.m
+++ SampleApp/AppDelegate.m
@@ -7,6 +7,7 @@
 
 #import "AppDelegate.h"
 #import "GTStaticTest.h"
+#import <execinfo.h>
 
 @interface AppDelegate ()
 
@@ -16,10 +17,12 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+	// Override point for customization after application launch.
 //    static
 //    [[GTStaticTest alloc] init];
-    return YES;
+	[self _caughtException];
+	[ @[].mutableCopy addObject: nil];
+	return YES;
 }
 
 
@@ -27,16 +30,48 @@
 
 
 - (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+	// Called when a new scene session is being created.
+	// Use this method to select a configuration to create the new scene with.
+	return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
 }
 
 
 - (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+	// Called when the user discards a scene session.
+	// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+	// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+}
+
+#pragma mark -crash
+-(void)_caughtException {
+    //Exception
+	NSSetUncaughtExceptionHandler(HandleNSException);
+    
+    //signal
+    signal(SIGABRT, SignalExceptionHandler);
+    signal(SIGILL, SignalExceptionHandler);
+    signal(SIGSEGV, SignalExceptionHandler);
+    signal(SIGFPE, SignalExceptionHandler);
+    signal(SIGBUS, SignalExceptionHandler);
+    signal(SIGPIPE, SignalExceptionHandler);
+}
+
+void HandleNSException(NSException *exception){
+	__unused NSString *reason = [exception reason];
+	__unused NSString *name = [exception name];
+    //存储crash信息
+}
+
+void SignalExceptionHandler(int signal){
+    void* callstack[128];
+    int frames = backtrace(callstack, 128);
+    char **strs = backtrace_symbols(callstack, 128);
+    NSMutableArray *backtrace = [NSMutableArray arrayWithCapacity:frames];
+    for (int i = 0; i < frames; i++) {
+        [backtrace addObject: [NSString stringWithUTF8String:strs[i]]];
+    }
+    free(strs);
+    //存储crash信息
 }
 
 
--- SampleApp/MIneController/GTLogin.m
+++ SampleApp/MIneController/GTLogin.m
@@ -6,9 +6,11 @@
 //
 
 #import "GTLogin.h"
+#import <TencentOpenAPI/TencentOAuth.h>
+#import <TencentOpenAPI/QQApiInterface.h>
 
-@interface GTLogin()
-
+@interface GTLogin()<TencentSessionDelegate>
+@property(nonatomic,strong,readwrite)TencentOAuth *oauth;
 @property(nonatomic,assign,readwrite)BOOL isLogin;
 @property(nonatomic,copy,readwrite)GTLoginFinishBlock finishBlock;
 
@@ -29,23 +31,71 @@
     self = [super init];
     if (self) {
         _isLogin = NO;
+        _oauth = [[TencentOAuth alloc] initWithAppId:@"222222" andDelegate:self];
     }
     return self;
 }
 
 -(BOOL)isLogin{
+    //登录状态失效的逻辑
     return _isLogin;
 }
 
 -(void)loginWithFinishBlocck:(GTLoginFinishBlock)finishiBlock{
-    
+    _finishBlock = [finishiBlock copy];
+    _oauth.authMode = kAuthModeClientSideToken;
+    [_oauth authorize:@[
+        kOPEN_PERMISSION_GET_USER_INFO,
+        kOPEN_PERMISSION_GET_SIMPLE_USER_INFO,
+        kOPEN_PERMISSION_ADD_ALBUM,
+        kOPEN_PERMISSION_ADD_ONE_BLOG,
+        kOPEN_PERMISSION_ADD_SHARE,
+        kOPEN_PERMISSION_ADD_TOPIC,
+        kOPEN_PERMISSION_CHECK_PAGE_FANS,
+        kOPEN_PERMISSION_GET_INFO,
+        kOPEN_PERMISSION_GET_OTHER_INFO,
+        kOPEN_PERMISSION_LIST_ALBUM,
+        kOPEN_PERMISSION_UPLOAD_PIC,
+        kOPEN_PERMISSION_GET_VIP_INFO,
+        kOPEN_PERMISSION_GET_VIP_RICH_INFO
+    ]];
 }
 
 -(void)logOut{
+    [_oauth logout:self];
     _isLogin = NO;
 }
 
+
+
 -(void)shareToQQWithArticleUrl:(NSURL *)articleUrl{
+    
+}
+
+- (void)tencentDidLogin {
+    _isLogin = YES;
+    [_oauth getUserInfo];
+}
+
+
+- (void)tencentDidNotLogin:(BOOL)cancelled {
+    if (_finishBlock) {
+        _finishBlock(NO);
+    }
+}
+
+- (void)tencentDidNotNetWork {
+    
+}
+
+-(void)getUserInfoResponse:(APIResponse *)response{
+    NSDictionary *userInfo = response.jsonResponse;
+    _nick = userInfo[@"nickName"];
+    _address = userInfo[@"city"];
+    _avatarUrl = userInfo[@"figureurl_qq_2"];
+    if (_finishBlock) {
+        _finishBlock(YES);
+    }
     
 }
 
